85cd2390b3094bd13f68333667df7fa49cfb2eb4
Mahdi Hassan <mahdioffi333@gmail.com>
2025-09-22 18:36:17 +0600

    Get All Users

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -4,12 +4,22 @@ import { userService } from "./user.service";
 const createUser = async (req: Request, res: Response) => {
     try {
         const result = await userService.createUser(req.body);
-        res.send(result);
+        res.status(201).send(result);
     } catch (error) {
-        console.log(error);
+        res.status(500).json(error);
+    }
+}
+
+const getAllFromDB = async (req: Request, res: Response) => {
+    try {
+        const result = await userService.getAllFromDB();
+        res.status(201).send(result);
+    } catch (error) {
+        res.status(500).json(error);
     }
 }
 
 export const userController = {
-    createUser
+    createUser,
+    getAllFromDB
 };
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,14 +1,31 @@
 import { prisma } from "../../config/db";
+import { Prisma, User } from "@prisma/client";
 
-const createUser = async(payload: any)=> {
-    console.log({payload});
+const createUser = async(payload: Prisma.UserCreateInput): Promise<User>=> {
     const createUser = await prisma.user.create({
         data: payload
     })
     return createUser;
 }
 
+const getAllFromDB = async() => {
+    const result = await prisma.user.findMany({
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            phone: true,
+            picture: true,
+            createdAt: true,
+            updatedAt: true,
+            role: true,
+            status: true
+        }
+    });
+    return result;
+}
 
 export const userService = {
-    createUser
+    createUser,
+    getAllFromDB
 };

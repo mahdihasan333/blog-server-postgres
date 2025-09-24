6e1bdd1f4598be6abc313199855522e227c33d65
Mahdi Hassan <mahdioffi333@gmail.com>
2025-09-24 18:12:44 +0600

    Filtering Data with Prisma (Part 1)

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -14,8 +14,11 @@ const getAllPosts = async(req: Request, res: Response) => {
     try {
         const page = Number(req.query.page) || 1;
         const limit = Number(req.query.limit) || 10;
-        const search = (req.query.search as string) || ""
-        const result = await PostService.getAllPosts({ page, limit, search });
+        const search = (req.query.search as string) || "";
+        const isFeatured = req.query.isFeatured ? req.query.isFeatured === 'true' : undefined;
+
+
+        const result = await PostService.getAllPosts({ page, limit, search, isFeatured });
         res.status(200).json(result);
     } catch (error) {
         res.status(500).send(error);
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,91 +1,95 @@
-import { Post, Prisma } from "@prisma/client"
-import { prisma } from '../../config/db';
+import { Post, Prisma } from "@prisma/client";
+import { prisma } from "../../config/db";
 
-const createPost = async(payload: Prisma.PostCreateInput): Promise<Post> => {
-    const createPost = await prisma.post.create({
-        data: payload,
-        include: {
-            author: {
-                select: {
-                    id: true,
-                    name: true,
-                    email: true
-                }
-            }
-        }
-    })
-    return createPost;
-}
+const createPost = async (payload: Prisma.PostCreateInput): Promise<Post> => {
+  const createPost = await prisma.post.create({
+    data: payload,
+    include: {
+      author: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      },
+    },
+  });
+  return createPost;
+};
 
-
-const getAllPosts = async({
-    page, limit, search
+const getAllPosts = async ({
+  page = 1,
+  limit = 10,
+  search,
+  isFeatured,
 }: {
-    page: number,
-    limit: number,
-    search: string
+  page?: number;
+  limit?: number;
+  search?: string;
+  isFeatured?: boolean;
 }) => {
-    const skip = (page - 1) * limit;
-    const result = await prisma.post.findMany({
-        skip,
-        take: limit,
-        where: {
+  const skip = (page - 1) * limit;
+
+  const where: any = {
+    AND: [
+        search && {
             OR: [
-                {
-                    title: {
-                        contains: search,
-                        mode: 'insensitive'
-                    }
-                },
-                {
-                    content: {
-                        contains: search,
-                        mode: 'insensitive'
-                    }
-                }
+                { title: { contains: search, mode: 'insensitive' } },
+                {content: { contains: search, mode: 'insensitive' } }
             ]
-        }
-    });
-    return result;
-}
-
-
-const getPostById = async(id: number) => {
-    const result = await prisma.post.findUnique({
-        where: {
-            id
         },
-        include: {
-            author: true
-        }
-    })
-    return result;
-}
-
-const updatePost = async(id: number, payload: Prisma.PostUpdateInput): Promise<Post> => {
-    const result = await prisma.post.update({
-        where: {
-            id
-        },
-        data: payload
-    })
-    return result;
-}
+        typeof isFeatured === 'boolean' && { isFeatured }
+    ].filter(Boolean)
+  }
 
 
-const deletePost = async(id: number) => {
-    const result = await prisma.post.delete({
-        where: {
-            id
-        }
-    })
-    return result;
-}
+
+  const result = await prisma.post.findMany({
+    skip,
+    take: limit,
+    where,
+  });
+  return result;
+};
+
+const getPostById = async (id: number) => {
+  const result = await prisma.post.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      author: true,
+    },
+  });
+  return result;
+};
+
+const updatePost = async (
+  id: number,
+  payload: Prisma.PostUpdateInput
+): Promise<Post> => {
+  const result = await prisma.post.update({
+    where: {
+      id,
+    },
+    data: payload,
+  });
+  return result;
+};
+
+const deletePost = async (id: number) => {
+  const result = await prisma.post.delete({
+    where: {
+      id,
+    },
+  });
+  return result;
+};
 
 export const PostService = {
-    createPost,
-    getAllPosts,
-    getPostById,
-    updatePost,
-    deletePost
-}
+  createPost,
+  getAllPosts,
+  getPostById,
+  updatePost,
+  deletePost,
+};
